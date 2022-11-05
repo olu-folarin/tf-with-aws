3e1e13681a3ee655602a44a56d32ae7e9463c846
folarin <folami25@gmail.com>
2022-11-05 16:43:54 +0100

    added an IAM user

--- main1.tf
+++ main1.tf
@@ -10,3 +10,11 @@ terraform {
 provider {
   region = var.region
 }
+
+# identityand access management config for the admin
+resource "aws_iam_user" "folarin" {
+    name = "folarin"
+    tags = {
+      description = "Technical Team Leader and migration craftsman"
+    }
+}
